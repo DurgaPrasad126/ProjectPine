02ecb11b3bd480c8fcd0072076bc0df1c6be6b1c
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-06-12 13:52:33 -0600

    Time: 1 ms (70.29%) | Memory: 42 MB (53.15%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -1,35 +1,29 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for(int i=0;i<arr2.length;i++){
-            map.put(arr2[i], i);
+        int max =  Arrays.stream(arr1).max().orElse(0);
+        int[] count  = new int[max+1];
+        int[] res = new int[arr1.length];
+        int pos = 0;
+        for(int e :arr1) {
+            count[e]++;
         }
 
-        for(int i=0;i<arr1.length-1;i++){
-            for(int j=i+1;j<arr1.length;j++){
-                if(!map.containsKey(arr1[i]) || !map.containsKey(arr1[j])) {
-                    if(map.containsKey(arr1[j])){
-                        int temp = arr1[j];
-                        arr1[j] = arr1[i];
-                        arr1[i] = temp;
-                    }
-
-                    if(!map.containsKey(arr1[i]) && !map.containsKey(arr1[j])) {
-                        if(arr1[i] > arr1[j]) {
-                            int temp = arr1[i];
-                            arr1[i] = arr1[j];
-                            arr1[j] = temp;
-                        }
-                    }
-                }
-                else if(map.get(arr1[i]) > map.get(arr1[j])){
-                    int temp = arr1[i];
-                    arr1[i] = arr1[j];
-                    arr1[j] = temp;
-                }
+        for(int e : arr2) {
+            while(count[e] > 0) {
+                res[pos++] = e;
+                count[e]--;
             }
         }
-        return arr1;
+
+        for(int i=0;i<=max;i++){
+            if(count[i] > 0) {
+                while(count[i] > 0) {
+                    res[pos++] = i;
+                    count[i]--;
+                }
+
+            }
+        }
+        return res;
     }
 }
