38b9fe9475a265b6a11812e077953fe9c2a7af6c
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-06-13 20:23:26 -0600

    Time: 36 ms (5.41%) | Memory: 44.7 MB (8.91%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
@@ -1,13 +1,35 @@
 class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
-        Arrays.sort(seats);
-        Arrays.sort(students);
+        //Arrays.sort(seats);
+        //Arrays.sort(students);
+
+        seats = countSort(seats);
+        students = countSort(students);
 
         int res = 0;
 
         for(int i=0;i<seats.length;i++){
+            System.out.println(seats[i] + " --- " + students[i]);
             res+=Math.abs(seats[i]-students[i]);
         }
         return res;
+    }
+
+    private int[] countSort(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[101];
+        int pos = 0;
+        for(int i=0;i<n;i++){
+            result[arr[i]]++;
+        }
+
+        for(int i=0;i<101;i++) {
+            while(result[i] > 0) {
+                arr[pos++] = i;
+                result[i]--;
+            }
+        }
+
+        return arr;
     }
 }
