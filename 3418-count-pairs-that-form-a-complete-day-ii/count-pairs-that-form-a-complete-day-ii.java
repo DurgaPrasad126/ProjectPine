95bc04cc0e40407dba9620d15b6105605b7b5be7
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-06-16 01:17:16 -0600

    Time: 21 ms (100.00%) | Memory: 94.4 MB (100.00%) - LeetSync

--- 3418-count-pairs-that-form-a-complete-day-ii/count-pairs-that-form-a-complete-day-ii.java
+++ 3418-count-pairs-that-form-a-complete-day-ii/count-pairs-that-form-a-complete-day-ii.java
@@ -1,25 +1,24 @@
 class Solution {
     public long countCompleteDayPairs(int[] hours) {
-        Map<Integer, List<Integer>> countPairs = new HashMap<>();
+        Map<Integer, Integer> countPairs = new HashMap<>();
         int n = hours.length;
         for(int i=0;i<n;i++) {
-            countPairs.computeIfAbsent(hours[i]%24, l->new ArrayList<>()).add(hours[i]);
+            countPairs.put(hours[i]%24, countPairs.getOrDefault(hours[i]%24, 0) + 1);
         }
-        System.out.println(countPairs);
         long completePairs = 0l;
         int[] counted = new int[24];
         for(Integer rem : countPairs.keySet()) {
             if(rem == 0 || rem == 12) {
                 counted[rem]=1;
-                int number = countPairs.get(rem).size();
+                int number = countPairs.get(rem);
                 completePairs+=(long)number*(number-1)/2;
             }
             else {
                 int remainingRemainder = 24-rem;
                 if(countPairs.containsKey(remainingRemainder)) {
                     if(counted[rem] == 1 || counted[remainingRemainder] == 1) continue;
-                    int remSize = countPairs.get(rem).size();
-                    int remainingRemainderSize = countPairs.get(remainingRemainder).size();
+                    int remSize = countPairs.get(rem);
+                    int remainingRemainderSize = countPairs.get(remainingRemainder);
                     completePairs+=(long)remSize*remainingRemainderSize;
                     counted[rem] = 1;
                     counted[remainingRemainder] = 1;
