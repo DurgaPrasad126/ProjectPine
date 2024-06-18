444663817849f71ccb8512e074671dc7942268d9
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-06-18 04:40:24 -0600

    Time: 405 ms (12.40%) | Memory: 46.5 MB (6.63%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.java
+++ 853-most-profit-assigning-work/most-profit-assigning-work.java
@@ -1,16 +1,45 @@
 class Solution {
-    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        List<Pair<Integer, Integer>> jobs = new ArrayList<>();
-        int N = profit.length, res = 0, i = 0, best = 0;
-        for (int j = 0; j < N; ++j)
-            jobs.add(new Pair<Integer, Integer>(difficulty[j], profit[j]));
-        Collections.sort(jobs, Comparator.comparing(Pair::getKey));
-        Arrays.sort(worker);
-        for (int ability : worker) {
-            while (i < N && ability >= jobs.get(i).getKey())
-                best = Math.max(jobs.get(i++).getValue(), best);
-            res += best;
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] workers) {
+        int maxProfitObtained = 0;
+        int n = difficulty.length;
+        int[][] diffProfit = new int[n][2];
+        int maxProfit = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            diffProfit[i][0] = difficulty[i];
+            diffProfit[i][1] = profit[i];
         }
-        return res;
+        for(int i=0;i<n;i++){
+            System.out.println(diffProfit[i][0] + " ---- " + diffProfit[i][1]);
+        }
+
+        Arrays.sort(diffProfit, (a,b) -> a[0]-b[0]);
+        for(int i=0;i<n-1;i++){
+            diffProfit[i+1][1] = Math.max(diffProfit[i][1], diffProfit[i+1][1]);
+            System.out.println(diffProfit[i][0] + " ---- " + diffProfit[i][1]);
+        }
+        //Arrays.sort(workers);
+        for(int worker : workers) {
+            int nearestDiffIndex = getNearestDifficultyIndex(diffProfit, worker);
+            maxProfitObtained+=nearestDiffIndex;
+        }
+
+        return maxProfitObtained;
+    }
+
+    private int getNearestDifficultyIndex(int[][] diffProfit, int worker){
+        int low = 0;
+        int high = diffProfit.length-1;
+        int maxProfit = 0;
+        while(low<=high) {
+            int mid = low+ (high-low)/2;
+            if(diffProfit[mid][0] <= worker) {
+                maxProfit = Math.max(maxProfit, diffProfit[mid][1]);
+                low = mid+1;
+            }
+            else {
+                high = mid-1;
+            }
+        }
+        return maxProfit;
     }
 }
