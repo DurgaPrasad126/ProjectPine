b3208ba1f60b3d8667acbd0987f4b9f6935e728e
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-07-03 16:40:40 -0600

    Time: 15 ms (96.33%) | Memory: 57 MB (61.61%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -1,22 +1,15 @@
 class Solution {
     public int minDifference(int[] nums) {
-        if(nums.length <= 4) return 0;
+        int n = nums.length;
+        if(n <= 4) return 0;
 
         Arrays.sort(nums);
 
+        int minDifference = Integer.MAX_VALUE;
 
-        int n = nums.length;
-        int a = nums[1]-nums[0];
-        int b = nums[2] - nums[1];
-        int c = nums[3] -nums[2];
-        int f = nums[n-1]-nums[n-2];
-        int e = nums[n-2]-nums[n-3];
-        int d = nums[n-3]-nums[n-4];
-
-        int diff = (a+b+c > a+b+f) ? a+b+c : a+b+f;
-        diff = diff > a+f+e ? diff : a+f+e;
-        diff = diff > d+e+f ? diff : d+e+f;
-
-        return nums[n-1]-nums[0]-diff;
+        for(int left = 0, right = n-4;left<4;left++,right++){
+            minDifference = Math.min(minDifference, nums[right]-nums[left]);
+        }
+        return minDifference;
     }
 }
