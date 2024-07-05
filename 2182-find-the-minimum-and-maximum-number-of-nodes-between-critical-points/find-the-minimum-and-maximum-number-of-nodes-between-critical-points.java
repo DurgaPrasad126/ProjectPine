497c67f20e77eed36a572f9ad8172e6ca5e47f55
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-07-04 21:43:42 -0600

    Time: 4 ms (100.00%) | Memory: 60.6 MB (54.76%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -18,9 +18,6 @@ class Solution {
         ListNode currPointer = head.next;
 
         int counter = 2;
-
-        //ArrayList<Integer> criticalPoints = new ArrayList<>();
-
         while(currPointer.next != null) {
             if((prevPointer.val < currPointer.val && currPointer.val > currPointer.next.val) 
             || (prevPointer.val > currPointer.val && currPointer.val < currPointer.next.val)) {
@@ -32,30 +29,13 @@ class Solution {
                     minDistance = Math.min(minDistance, counter-prevCritical);
                     prevCritical = counter;
                 }
-                //criticalPoints.add(counter);
             }
             prevPointer=prevPointer.next;
             currPointer = currPointer.next;
             counter++;
         }
 
-        /*Collections.sort(criticalPoints);
-        int criticSize = criticalPoints.size();
-        if(criticSize < 2) return new int[]{-1,-1};
-        int minima = Integer.MAX_VALUE;
-        int maxima = Integer.MIN_VALUE;
-        int minDiff = Integer.MAX_VALUE;
-
-        for(int i=0;i<criticSize-1;i++) {
-            minima = Math.min(minima, criticalPoints.get(i));
-            maxima = Math.max(maxima, criticalPoints.get(i));
-            minDiff = Math.min(minDiff, criticalPoints.get(i+1)-criticalPoints.get(i));
-        }
-                    minima = Math.min(minima, criticalPoints.get(criticSize-1));
-            maxima = Math.max(maxima, criticalPoints.get(criticSize-1));
-            */
-
-            if(firstCritical == -1 || prevCritical == -1 || (firstCritical == prevCritical)) return new int[]{-1,-1};
+        if(firstCritical == -1 || prevCritical == -1 || (firstCritical == prevCritical)) return new int[]{-1,-1};
             
         return new int[]{minDistance, prevCritical-firstCritical};
     }
