6b2734ac67261f2440df2b0b22e1f6a119f88b8c
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-07-20 23:04:33 -0600

    Time: 5 ms (77.78%) | Memory: 53 MB (40.87%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
@@ -6,7 +6,7 @@ class Solution {
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                int curEle = Math.min(rowSum[i], colSum[j]);
+                int curEle = rowSum[i] < colSum[j] ? rowSum[i] : colSum[j];
                 matrix[i][j] = curEle;
                 rowSum[i]-=curEle;
                 colSum[j]-=curEle;
