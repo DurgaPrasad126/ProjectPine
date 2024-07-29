3f0c471d961283aaf2f06cb9415c69a0daaa5522
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-07-29 17:01:02 -0600

    Time: 1994 ms (14.11%) | Memory: 42.1 MB (87.41%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.java
+++ 1511-count-number-of-teams/count-number-of-teams.java
@@ -1,37 +1,15 @@
 class Solution {
-    //editorial
     public int numTeams(int[] rating) {
-        if(rating == null || rating.length == 0){
-            return 0;
-        }
-        
-        int len = rating.length;
-        int[] dp = new int[len];
-        int count = 0;
-        
-        // first <r[i] <r[j] <r[k] order
-        for(int i = 0; i < len; i++){
-            for(int j = i; j >= 0; j--){
-                if(rating[i] > rating[j]){
-                    dp[i]++;
-                    count = count + dp[j];
+        int teamsCount = 0;
+        int n = rating.length;
+        for(int i=0;i<n-2;i++){
+            for(int j=i+1;j<n-1;j++){
+                for(int k=j+1;k<n;k++){
+                    if((rating[i] > rating[j] && rating[j] > rating[k]) 
+                    || (rating[i] < rating[j] && rating[j] < rating[k])) teamsCount++;
                 }
             }
         }
-        
-        //System.out.println(count);
-        dp = new int[len];
-        
-        for(int i = 0; i < len; i++){
-            for(int j = i; j >= 0; j--){
-                if(rating[i] < rating[j]){
-                    dp[i]++;
-                    count = count + dp[j];
-                }
-            }
-        }
-        
-        return count;
-        
+        return teamsCount;
     }
 }
