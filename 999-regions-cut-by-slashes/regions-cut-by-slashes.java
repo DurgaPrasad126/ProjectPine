ebfb12ea14b4e54f832d7c7b11d11920940b4325
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-08-10 19:19:01 -0600

    Time: 8 ms (38.81%) | Memory: 42.4 MB (42.92%) - LeetSync

--- 999-regions-cut-by-slashes/regions-cut-by-slashes.java
+++ 999-regions-cut-by-slashes/regions-cut-by-slashes.java
@@ -1,49 +1,48 @@
 class Solution {
     public int regionsBySlashes(String[] grid) {
-        int[][] mat = new int[grid.length * 3][grid.length * 3];
-        boolean[][] visited = new boolean[mat.length][mat.length];
-        int count = 0;
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[i].length(); j++){
-                char ch = grid[i].charAt(j);
-                if(ch == '/'){
-                    int r = i * 3;
-                    int c = (j * 3) + 2;
-                    mat[r][c] = 1;
-                    mat[r + 1][c - 1] = 1;
-                    mat[r + 2][c - 2] = 1;
-                }else if(ch == '\\'){
-                    int r = i * 3;
-                    int c = j * 3;
-                    mat[r][c] = 1;
-                    mat[r + 1][c + 1] = 1;
-                    mat[r + 2][c + 2] = 1;
+        int n =  grid.length;
+        int[][] adjM = new int[3*n][3*n];
+
+        for(int i=0;i<n;i++){
+            
+            for(int j=0;j<n;j++){
+                if(grid[i].charAt(j) == '\\'){
+                    int ni = 3*i;
+                    int nj = 3*j;
+                    adjM[ni][nj] = 1;
+                    adjM[ni+1][nj+1] = 1;
+                    adjM[ni+2][nj+2] = 1;
+                }
+                else if(grid[i].charAt(j) == '/'){
+                    int ni = 3*i+2;
+                    int nj = 3*j+2;
+                    adjM[ni][nj-2]=1;
+                    adjM[ni-1][nj-1] = 1;
+                    adjM[ni-2][nj] = 1;
                 }
             }
         }
-        // for(int[] ar : mat){
-        //     System.out.println(Arrays.toString(ar));
-        // }
 
-        for(int i = 0; i < mat.length; i++){
-            for(int j = 0; j < mat[0].length; j++){
-                if(!visited[i][j] && mat[i][j] != 1){
-                    dfs(i, j, visited, mat);
+        int count = 0;
+        for(int i=0; i<3*n;i++){
+            for(int j=0;j<3*n;j++){
+                if(adjM[i][j] == 0) {
+                    DFS(adjM, i, j, 3*n);
                     count++;
                 }
             }
         }
+
         return count;
     }
 
-    public void dfs(int r, int c, boolean[][] visited, int[][] mat){
-        if(r < 0 || r == mat.length || c < 0 || c == mat.length || visited[r][c] || mat[r][c] == 1){
-            return;
-        }
-        visited[r][c] = true;
-        dfs(r + 1, c, visited, mat);
-        dfs(r - 1, c, visited, mat);
-        dfs(r, c + 1, visited, mat);
-        dfs(r, c - 1, visited, mat);
+    public void DFS(int[][] adjM, int i, int j, int n){
+        if(i < 0 || j < 0 || i>=n || j >= n || adjM[i][j] == 1) return;
+
+        adjM[i][j] = 1;
+        DFS(adjM, i+1, j, n);
+        DFS(adjM, i-1, j, n);
+        DFS(adjM, i, j-1, n);
+        DFS(adjM, i, j+1, n);
     }
 }
