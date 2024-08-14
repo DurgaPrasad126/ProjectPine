12ceb2b71fe1cc253578bb6202ed57192d048c2c
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-08-14 09:49:11 -0600

    Time: 402 ms (5.44%) | Memory: 44.8 MB (9.35%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
@@ -1,8 +1,7 @@
 class Solution {
     public int smallestDistancePair(int[] nums, int k) {
-        if(nums.length == 10000 && nums[0] == 197180) return 292051;
-        Map<Integer, Integer> eleMap = new HashMap<>();
-        Map<Integer, Integer> freqMap = new TreeMap<>();
+        /*Map<Integer, Integer> eleMap = new HashMap<>();
+        Map<Integer, Integer> freqMap = new HashMap<>();
 
 
         for(int i : nums) {
@@ -14,10 +13,13 @@ class Solution {
         Set<Integer> keySet = eleMap.keySet();
         Integer[] keyArr = new Integer[keySet.size()];
         keySet.toArray(keyArr);
+        int setl = keySet.size();
 
-        for(int i=0;i<keySet.size();i++){
-            for(int j=i+1;j<keySet.size();j++){
-                int diff = Math.abs(keyArr[i]-keyArr[j]);
+        System.out.println(keySet.size());
+
+        for(int i=0;i<setl;i++){
+            for(int j=i+1;j<setl;j++){
+                int diff = keyArr[i]-keyArr[j] < 0 ? keyArr[j] - keyArr[i] : keyArr[i]-keyArr[j];
                 freqMap.put(diff, freqMap.getOrDefault(diff, 0) 
                                     + (eleMap.get(keyArr[i])*eleMap.get(keyArr[j])));
             }
@@ -25,7 +27,9 @@ class Solution {
                                     + (eleMap.get(keyArr[i])*(eleMap.get(keyArr[i])-1)/2));
         }
 
-        //System.out.println(freqMap);
+        System.out.println(freqMap.size());
+
+        //if(nums.length == 10000 && nums[0] == 197180) return 292051;
 
         int count = 0;
         for(int i : freqMap.keySet()){
@@ -36,5 +40,27 @@ class Solution {
         }
         
         return -1;
+        */
+
+        int size = nums.length;
+        int maxEle = 0;
+        for(int i=0;i<size;i++){
+            if(nums[i] > maxEle) maxEle = nums[i];
+        }
+
+        int[] freqArr = new int[maxEle+1];
+
+        for(int i=0;i<size;i++){
+            for(int j=i+1; j<size;j++){
+                ++freqArr[Math.abs(nums[i] - nums[j])];
+            }
+        }
+
+        for(int i=0;i<maxEle+1;i++){
+            k-=(freqArr[i]);
+            if(k<=0) return i;
+        }
+        return -1;
+
     }
 }
