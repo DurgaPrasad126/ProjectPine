9f2150d40341e23430e3e5615a7bc170f371a775
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-08-14 23:03:19 -0600

    Time: 2 ms (95.27%) | Memory: 56.1 MB (18.72%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -1,18 +1,15 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int[] change = new int[3];
+        int[] change = new int[2];
         
         for(int bill : bills) {
             if(bill == 5){
                 change[0]++;
-                //System.out.println(change[0] + " --- " + change[1] + " --- " + change[2]);
             }
             else if(bill == 10) {
                 if(change[0] <= 0) return false;
                 change[0]--; 
                 change[1]++;
-                //System.out.println(change[0] + " --- " + change[1] + " --- " + change[2]);
-
             }
             else {
                 if((change[1] >= 1 && change[0] >= 1) || change[0] >= 3) 
@@ -22,8 +19,6 @@ class Solution {
                         change[0]--;
                     }
                     else if(change[1] < 1 && change[0] >= 3) change[0]-=3;
-                    change[2]++;
-                    //System.out.println(change[0] + " --- " + change[1] + " --- " + change[2]);
                 }
                 else return false;
             }
