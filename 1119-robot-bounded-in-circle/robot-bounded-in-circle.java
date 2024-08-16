295024ec50457ef0aae5a8be57cb6002e12ebd3c
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-08-15 23:08:30 -0600

    Time: 0 ms (100.00%) | Memory: 41.4 MB (43.56%) - LeetSync

---
+++ 1119-robot-bounded-in-circle/robot-bounded-in-circle.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isRobotBounded(String instructions) {
+        int[][] directions = {{0,1}, {-1,0}, {0,-1}, {1,0}};
+        char[] ch = instructions.toCharArray();
+        int[] curPos = {0,0};
+        char curDir = 'N';
+        for(int i=0;i<ch.length;i++){
+            if(ch[i] == 'G'){
+                if(curDir == 'N') curPos[1]++;
+                else if(curDir == 'E') curPos[0]++;
+                else if(curDir == 'W') curPos[0]--;
+                else if(curDir == 'S') curPos[1]--;
+            }
+            else if(ch[i] == 'L') {
+                if(curDir == 'N') curDir = 'W';
+                else if(curDir == 'E') curDir = 'N';
+                else if(curDir == 'W') curDir = 'S';
+                else if(curDir == 'S') curDir = 'E';
+            }
+            else if(ch[i] == 'R') {
+                if(curDir == 'N') curDir = 'E';
+                else if(curDir == 'E') curDir = 'S';
+                else if(curDir == 'W') curDir = 'N';
+                else if(curDir == 'S') curDir = 'W';
+            } 
+        }
+        return (curPos[0] == 0 && curPos[1] == 0) || curDir != 'N';
+    }
+}
