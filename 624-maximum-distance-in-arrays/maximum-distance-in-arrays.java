6ae6eb627d4e123b078eb0cb38e73132ea03d17a
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-08-15 22:13:52 -0600

    Time: 9 ms (63.29%) | Memory: 64.4 MB (55.27%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.java
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxDistance(List<List<Integer>> arrays) {
         int maxDist = 0;
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> b[1]-a[1]);
+        /*PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> b[1]-a[1]);
         PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> a[1]-b[1]);
         List<Integer> array = new ArrayList<>();
         int len = 0;
@@ -24,5 +24,48 @@ class Solution {
             else min = minHeap.poll();
         } 
         return max[1]-min[1]; 
+        */
+
+        int n = arrays.size();
+        int[][] twoMin = new int[2][2];
+        twoMin[0][1] = Integer.MAX_VALUE;
+        twoMin[1][1] = Integer.MAX_VALUE;
+        int[][] twoMax = new int[2][2];
+        twoMax[0][1] = Integer.MIN_VALUE;
+        twoMax[1][1] = Integer.MIN_VALUE;
+
+        for(int i=0;i<n;i++){
+            if(twoMin[0][1] > arrays.get(i).get(0)) {
+                twoMin[1][1] = twoMin[0][1];
+                twoMin[1][0] = twoMin[0][0];
+                twoMin[0][1] = arrays.get(i).get(0);
+                twoMin[0][0] = i;
+            }
+            else if(twoMin[1][1] > arrays.get(i).get(0)){
+                twoMin[1][1] = arrays.get(i).get(0);
+                twoMin[1][0] = i;
+            }
+
+            List<Integer> array = arrays.get(i);
+            int le = array.size();
+
+            if(twoMax[0][1] < array.get(le-1)) {
+                twoMax[1][1] = twoMax[0][1];
+                twoMax[1][0] = twoMax[0][0];
+                twoMax[0][1] = array.get(le-1);
+                twoMax[0][0] = i;
+            }
+            else if(twoMax[1][1] < array.get(le-1)){
+                twoMax[1][1] = array.get(le-1);
+                twoMax[1][0] = i;
+            }
+
+        }
+
+        if(twoMax[0][0] == twoMin[0][0]) {
+            return Math.max(twoMax[1][1]-twoMin[0][1], twoMax[0][1]-twoMin[1][1]);
+        }
+
+        return twoMax[0][1] - twoMin[0][1];
     }
 }
