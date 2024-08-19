cafe9679c5e1bb301c1cf98362e66396aae0d22e
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-08-19 00:33:40 -0600

    Time: 16 ms (38.98%) | Memory: 40.5 MB (55.42%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -3,14 +3,12 @@ class Solution {
         if(n==1) return 0;
         if(n==2) return 2;
 
-        return 1+minimumSteps(n, 1, 1);
+        return 1+minimumSteps(n, 0, 1,1);
     }
-    public int minimumSteps(int n, int curLen, int prevLen) {
-        if(curLen == n) {
-            return 0;
-        }
-        if(curLen > n) return 1000;
+    public int minimumSteps(int n, int steps, int aCount, int curCount) {
+        if(n == curCount) return steps;
+        if(n < curCount) return 1000;
 
-        return Math.min(2+minimumSteps(n, curLen*2, curLen), 1+minimumSteps(n, curLen+prevLen, prevLen));
+        return Math.min(minimumSteps(n, steps+1, aCount, curCount+aCount), minimumSteps(n, steps+2, curCount, 2*curCount));
     }
 }
