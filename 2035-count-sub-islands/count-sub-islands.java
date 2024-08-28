bf44ca0b40ed9b93cca3eb0675143e7678aefa80
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-08-28 08:47:48 -0600

    Time: 26 ms (41.04%) | Memory: 88.8 MB (13.24%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.java
+++ 2035-count-sub-islands/count-sub-islands.java
@@ -1,26 +1,33 @@
 class Solution {
     public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int m = grid1.length;
+        int n = grid1[0].length;
+        boolean[][] isVisited = new boolean[m][n];
         int res = 0;
-        for(int i=0;i<grid1.length;i++){
-            for(int j=0;j<grid1[0].length;j++){
-                if(grid2[i][j] == 1 && dfs(grid1, grid2, i, j)){
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid2[i][j] == 1 && !isVisited[i][j] && dfs(grid1, grid2, i, j, m, n, isVisited)){
                     res++;
                 }
             }
         }
+
         return res;
     }
+    public boolean dfs(int[][] grid1, int[][] grid2, int i, int j, int m, int n, boolean[][] isVisited) {
+        if(i<0 || i>= m || j< 0 || j>= n 
+            || grid2[i][j] == 0 || isVisited[i][j]) 
+                return true;
 
-    public boolean dfs(int[][] grid1, int[][] grid2, int i, int j){
-        if(i<0 || i>= grid1.length || j<0 || j>= grid1[0].length || grid2[i][j] == 0) return true;
         if(grid2[i][j] != grid1[i][j]) return false;
-        grid2[i][j] = 0;
+        
+        isVisited[i][j] = true;
 
-        boolean left = dfs(grid1, grid2, i, j-1);
-        boolean right = dfs(grid1, grid2, i, j+1);
-        boolean top = dfs(grid1, grid2, i-1, j);
-        boolean bottom = dfs(grid1, grid2, i+1, j);
-
-        return left && right && top && bottom;
+        boolean top = dfs(grid1, grid2, i-1, j, m, n, isVisited);
+        boolean down = dfs(grid1, grid2, i+1, j, m, n, isVisited);
+        boolean left = dfs(grid1, grid2, i, j-1, m, n, isVisited);
+        boolean right = dfs(grid1, grid2, i, j+1, m, n, isVisited);
+        return top && down && left && right;
     }
 }
