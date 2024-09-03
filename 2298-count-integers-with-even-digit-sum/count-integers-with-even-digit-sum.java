f81d86eaa601219a2f568c474d2f8edad2019d1f
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-09-02 20:27:36 -0600

    Time: 0 ms (100.00%) | Memory: 40.1 MB (76.17%) - LeetSync

--- 2298-count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.java
+++ 2298-count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.java
@@ -1,15 +1,10 @@
 class Solution {
     public int countEven(int num) {
-        int count = 0, sum=0, digit=0;
-        for(int i=1; i<= num;i++) {
-            sum =0;
-            digit=i;
+        int digit=num,sum=0;
             while(digit>0){
                 sum+=digit%10;
                 digit/=10;
             }
-            if(sum%2==0) count++;
-        }
-        return count;
+        return sum%2==0?num/2:(num-1)/2;
     }
 }
