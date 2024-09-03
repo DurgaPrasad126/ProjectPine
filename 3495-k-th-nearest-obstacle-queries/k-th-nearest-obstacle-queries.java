8afc85a51e4deb9a4e66f8399d76f1afeacc5cf4
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-09-03 10:54:08 -0600

    Time: 114 ms (48.30%) | Memory: 142.5 MB (79.52%) - LeetSync

--- 3495-k-th-nearest-obstacle-queries/k-th-nearest-obstacle-queries.java
+++ 3495-k-th-nearest-obstacle-queries/k-th-nearest-obstacle-queries.java
@@ -6,21 +6,18 @@ class Solution {
         int pos = 0;
         for(int[] query : queries) {
             int distanceFromOrigin = Math.abs(query[0]) + Math.abs(query[1]);
-            if(maxHeap.size() >= k && distanceFromOrigin > maxHeap.peek()) {
-                res[pos++] = maxHeap.peek();
+            if(maxHeap.size() < k) {
+                maxHeap.offer(distanceFromOrigin);
             }
-            else {
-                if(maxHeap.size() < k) {
-                    maxHeap.offer(distanceFromOrigin);
-                    res[pos++] = maxHeap.size() == k ? maxHeap.peek() : -1;
-                }
-                else{
-                    maxHeap.poll();
-                    maxHeap.offer(distanceFromOrigin);
-                    res[pos++] = maxHeap.peek();
-                }
+            else if(distanceFromOrigin < maxHeap.peek()) {
+                maxHeap.poll();
+                maxHeap.offer(distanceFromOrigin);
             }
 
+            if(maxHeap.size() == k) {
+                res[pos++] = maxHeap.peek();
+            }
+            else res[pos++] = -1;
         }
         return res;
     }
