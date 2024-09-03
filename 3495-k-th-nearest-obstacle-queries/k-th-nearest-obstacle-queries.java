5ed8eb12882088ed1ff7adf0dbd04034cc0620e6
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-09-03 10:48:19 -0600

    Time: 116 ms (45.59%) | Memory: 169.1 MB (9.98%) - LeetSync

---
+++ 3495-k-th-nearest-obstacle-queries/k-th-nearest-obstacle-queries.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] resultsArray(int[][] queries, int k) {
+        int n = queries.length;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        int[] res = new int[n];
+        int pos = 0;
+        for(int[] query : queries) {
+            int distanceFromOrigin = Math.abs(query[0]) + Math.abs(query[1]);
+            if(maxHeap.size() >= k && distanceFromOrigin > maxHeap.peek()) {
+                res[pos++] = maxHeap.peek();
+            }
+            else {
+                if(maxHeap.size() < k) {
+                    maxHeap.add(distanceFromOrigin);
+                    res[pos++] = maxHeap.size() == k ? maxHeap.peek() : -1;
+                }
+                else{
+                    maxHeap.poll();
+                    maxHeap.add(distanceFromOrigin);
+                    res[pos++] = maxHeap.peek();
+                }
+            }
+
+        }
+        return res;
+    }
+}
