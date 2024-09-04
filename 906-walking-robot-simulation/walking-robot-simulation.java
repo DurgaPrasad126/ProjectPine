b87271f717d25b2afe9a0e540c8a90db434e44c1
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-09-03 21:50:57 -0600

    Time: 19 ms (68.56%) | Memory: 52.5 MB (21.40%) - LeetSync

---
+++ 906-walking-robot-simulation/walking-robot-simulation.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int robotSim(int[] commands, int[][] obstacles) {
+        int n = commands.length;
+        int m = obstacles.length;
+        int[] pos = new int[2];
+        int[][] dir = {{0,1}, {-1,0},{0,-1}, {1,0}};
+        int cur = 0;
+        Map<Integer,Set<Integer>> map = new HashMap<>();
+        int res = 0;
+        int xpre =0;
+        int ypre =0;
+        for(int i=0;i<m;i++){
+            map.computeIfAbsent(obstacles[i][0],l -> new HashSet<>()).add(obstacles[i][1]);
+        }
+        for(int i=0;i<n;i++) {
+            if(commands[i] == -1) {
+                if(cur-1<0) cur = 3;
+                else cur--;
+            }
+            else if(commands[i] == -2) {
+                if(cur+1>3) cur = 0;
+                else cur++;
+            }
+            else {
+                int x = dir[cur][0];
+                int y = dir[cur][1];
+
+                for(int j=0;j<commands[i];j++) {
+                    pos[0]+=x;
+                    pos[1]+=y;
+                    if(map.containsKey(pos[0]) && map.get(pos[0]).contains(pos[1])) {
+                        pos[0]-=x;
+                        pos[1]-=y;
+                        break;
+                    }
+
+                }
+                if(x!=0) xpre = pos[0]*pos[0];
+                if(y!=0) ypre = pos[1]*pos[1];
+
+                int temp = xpre+ypre;
+                if(res<temp)res = temp;
+            }
+
+            //System.out.println(pos[0]+" -- " + pos[1]);
+        }
+
+        return res;
+    }
+}
