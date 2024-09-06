bc3d108276f8597954a948917da58f273cf12819
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-09-05 18:39:58 -0600

    Time: 22 ms (27.77%) | Memory: 64.2 MB (76.55%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -10,6 +10,7 @@
  */
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
+        /*
         Set<Integer> set = new HashSet<>();
         for(int num : nums) set.add(num);
 
@@ -18,13 +19,11 @@ class Solution {
         ListNode prevPtr = dummy;
         ListNode iterator = dummy.next;
 
-                // Handle the case where the head node needs to be removed
         while (iterator != null && set.contains(iterator.val)) {
             iterator = iterator.next;
         }
         prevPtr.next = iterator;
 
-        // If the list is empty after removing head nodes, return null
         if (iterator == null) {
             return null;
         }
@@ -39,5 +38,35 @@ class Solution {
             iterator = iterator.next;
         }
         return dummy.next;
+        */
+
+         Set<Integer> valuesToRemove = new HashSet<>();
+        for (int num : nums) {
+            valuesToRemove.add(num);
+        }
+
+        // Handle the case where the head node needs to be removed
+        while (head != null && valuesToRemove.contains(head.val)) {
+            head = head.next;
+        }
+
+        // If the list is empty after removing head nodes, return null
+        if (head == null) {
+            return null;
+        }
+
+        // Iterate through the list, removing nodes with values in the set
+        ListNode current = head;
+        while (current.next != null) {
+            if (valuesToRemove.contains(current.next.val)) {
+                // Skip the next node by updating the pointer
+                current.next = current.next.next;
+            } else {
+                // Move to the next node
+                current = current.next;
+            }
+        }
+
+        return head;
     }
 }
