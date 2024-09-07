d401f976bab082c046e2d4d1ca93cd4c6f686906
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-09-07 16:50:09 -0600

    Time: 1 ms (97.89%) | Memory: 45 MB (39.47%) - LeetSync

--- 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.java
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.java
@@ -26,20 +26,18 @@
 class Solution {
     public boolean isSubPath(ListNode head, TreeNode root) {
     if (root == null) return false;
-    // Start DFS from the current node or try finding the path starting from left/right subtrees
     return dfs(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
 }
 
 private boolean dfs(ListNode head, TreeNode root) {
-    if (head == null) return true; // Reached the end of the linked list
-    if (root == null) return false; // Reached the end of a tree branch without matching the list
+    if (head == null) return true; 
+    if (root == null) return false;
 
-    // Match the current node's value and continue DFS if they match
     if (root.val == head.val) {
         return dfs(head.next, root.left) || dfs(head.next, root.right);
     }
 
-    return false; // Values don't match
+    return false;
 }
 
 }
