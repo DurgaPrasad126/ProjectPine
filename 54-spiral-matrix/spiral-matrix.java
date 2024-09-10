afcf736186e1bfd220269f2e3b9419c378582c7a
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-09-09 23:06:07 -0600

    Time: 7 ms (100.00%) | Memory: 41.7 MB (9.12%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int i=0, j=0, cur_d = 0;
+        int m = matrix.length, dm=m;
+        int n = matrix[0].length, dn=n, cur=0;
+        int[][] direction = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        Set<String> pos = new HashSet<>();
+
+        int[] res = new int[m*n];
+        while(cur < m*n) {
+            res[cur] = matrix[i][j];
+            pos.add(i+"_"+j);
+            int new_i = i + direction[cur_d][0];
+            int new_j = j + direction[cur_d][1];
+
+            if(new_i < 0 
+                || new_j < 0
+                || new_i >= dm
+                || new_j >= dn
+                || pos.contains(new_i+"_"+new_j)
+                ) {
+                    cur_d = (cur_d+1)%4;
+                }
+            
+            i+=direction[cur_d][0];
+            j+=direction[cur_d][1];
+
+            cur++;
+            
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for(int ij : res) result.add(ij);
+        return result;
+    }
+}
