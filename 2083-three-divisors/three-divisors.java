2da46d43f357bcf5236ec7ea7f65894e081edb52
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-09-16 12:06:59 -0600

    Time: 0 ms (100.00%) | Memory: 40.3 MB (71.42%) - LeetSync

---
+++ 2083-three-divisors/three-divisors.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isThree(int n) {
+        int sqrt = (int)Math.sqrt(n);
+        if(sqrt*sqrt != n) return false;
+        return isPrime(sqrt);
+    }
+    public boolean isPrime(int num) {
+        if(num<2) return false;
+        for(int i=2;i<num/2;i++){
+            if(num%i == 0) return false;
+        }
+        return true;
+    }
+}
