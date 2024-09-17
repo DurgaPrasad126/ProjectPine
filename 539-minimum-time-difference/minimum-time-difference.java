a2e6bb97233f39786cc44ffb2d28803f583d8fb0
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-09-17 17:31:08 -0600

    Time: 12 ms (28.21%) | Memory: 45 MB (82.08%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.java
+++ 539-minimum-time-difference/minimum-time-difference.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=1;i<timePoints.size();i++) {
             int min1 = Integer.valueOf(timePoints.get(i-1).substring(0,2)) * 60 + Integer.valueOf(timePoints.get(i-1).substring(3));
             int min2 = Integer.valueOf(timePoints.get(i).substring(0,2)) * 60 + Integer.valueOf(timePoints.get(i).substring(3));
-
+            if(min2-min1 == 0) return 0;
             if(min2-min1 < minDiff) minDiff = min2-min1;
         }
         int min2 = Integer.valueOf(timePoints.get(timePoints.size()-1).substring(0,2)) * 60 + Integer.valueOf(timePoints.get(timePoints.size()-1).substring(3));
