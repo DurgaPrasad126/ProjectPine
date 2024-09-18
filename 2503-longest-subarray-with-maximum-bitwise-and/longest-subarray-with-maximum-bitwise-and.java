867c79e5cb455db16ca2df7e82f60294095d2b2e
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-09-17 19:12:41 -0600

    Time: 2 ms (99.81%) | Memory: 59.1 MB (89.37%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
@@ -1,24 +1,24 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-       int max = 0;
-       int len = 0;
-       
-       for(int i = 0; i < nums.length; i++){
-           if(nums[i] > max){
-               max = nums[i];
-               len = 1;
-               while(i < nums.length - 1 && nums[i] == nums[i+1] ){
-                   i++; len++;
-               }
-           }else if(nums[i] == max){
-               int tempLen = 1;
-               while(i < nums.length - 1 && nums[i] == nums[i+1] ){
-                   i++; tempLen++;
-               }
-               len = Math.max(len, tempLen);
-           }
-       }
-       
-       return len;
-   }
+        int max = 0; int maxIdx = 0;
+        for(int i=0;i<nums.length;i++) {
+            if(max < nums[i]) {
+                max = nums[i];
+                maxIdx = i;
+            }
+        }
+
+        int maxLen = 0;
+        int len = 0;
+        while(maxIdx < nums.length) {
+            if(nums[maxIdx] != max) {
+                maxLen = Math.max(maxLen, len);
+                len = 0;
+            }
+            else len++;
+            maxIdx++;
+        }
+        return Math.max(maxLen,len);
+
+    }
 }
