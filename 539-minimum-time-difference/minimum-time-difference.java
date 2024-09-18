7b40932464543411794d3674fa9763891b99ed25
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-09-17 18:00:45 -0600

    Time: 2 ms (97.98%) | Memory: 45 MB (87.77%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.java
+++ 539-minimum-time-difference/minimum-time-difference.java
@@ -1,16 +1,28 @@
 class Solution {
     public int findMinDifference(List<String> timePoints) {
-        Collections.sort(timePoints);
-        int minDiff = Integer.MAX_VALUE;
-        for(int i=1;i<timePoints.size();i++) {
-            int min1 = Integer.valueOf(timePoints.get(i-1).substring(0,2)) * 60 + Integer.valueOf(timePoints.get(i-1).substring(3));
-            int min2 = Integer.valueOf(timePoints.get(i).substring(0,2)) * 60 + Integer.valueOf(timePoints.get(i).substring(3));
-            if(min2-min1 == 0) return 0;
-            if(min2-min1 < minDiff) minDiff = min2-min1;
-        }
-        int min2 = Integer.valueOf(timePoints.get(timePoints.size()-1).substring(0,2)) * 60 + Integer.valueOf(timePoints.get(timePoints.size()-1).substring(3));
-        int min1 = Integer.valueOf(timePoints.get(0).substring(0,2)) * 60 + Integer.valueOf(timePoints.get(0).substring(3));
+        int n = timePoints.size();
 
-        return Math.min(minDiff, 1440-(min2-min1));
+        int[] freq = new int[1440];
+
+        for(int i=0;i<n;i++) {
+            int minutes = Integer.valueOf(timePoints.get(i).substring(0,2))*60+Integer.valueOf(timePoints.get(i).substring(3));
+            if(freq[minutes] == 1) return 0;
+            freq[minutes]++;
+        }
+        int prevIdx = Integer.MAX_VALUE;
+        int firstIdx = Integer.MAX_VALUE;
+        int lastIdx = Integer.MAX_VALUE;
+        int minDiff = Integer.MAX_VALUE;
+
+        for(int i=0;i<1440;i++){
+            if(freq[i] == 1) {
+                if(prevIdx != Integer.MAX_VALUE) minDiff = Math.min(minDiff, i-prevIdx);
+                prevIdx = i;
+                if(firstIdx == Integer.MAX_VALUE) firstIdx = i;
+                lastIdx = i;
+            }
+        }
+
+        return Math.min(minDiff, 1440 - lastIdx + firstIdx);
     }
 }
