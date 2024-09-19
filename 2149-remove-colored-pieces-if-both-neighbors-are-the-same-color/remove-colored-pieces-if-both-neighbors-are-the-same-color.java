9ecb5fa03e10b0f3d75fd8631da1305b9003532e
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-09-19 09:10:14 -0600

    Time: 9 ms (88.53%) | Memory: 45.5 MB (18.24%) - LeetSync

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -3,8 +3,10 @@ class Solution {
         int aCube = 0, bCube = 0;
         char[] ch = colors.toCharArray();
         for(int i=0;i<colors.length()-2;i++) {
-            if(ch[i] == ch[i+1] && ch[i+1] == ch[i+2] && ch[i+2] == 'A') aCube++;
-            else if(ch[i] == ch[i+1] && ch[i+1] == ch[i+2] && ch[i+2] == 'B') bCube++;
+            if(ch[i] == ch[i+1] && ch[i+1] == ch[i+2]) {
+                if(ch[i+2] == 'A') aCube++;
+                else bCube++;
+            }
         }
         return aCube > bCube;
     }
