a1cbd6b564afe0fe86c43fa0bc939697ceedeea1
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-09-19 12:56:18 -0600

    Time: 4 ms (33.70%) | Memory: 60.4 MB (5.80%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
@@ -1,18 +1,22 @@
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
-        int ar[]=new int[queries.length];
-        int sum=0;
-        for(int i=0;i<queries.length;i++)
-        {
-            sum=0;
-           for(int j=queries[i][0];j<=queries[i][1];j++)
-           {
-               
-              sum^=arr[j];
-           }
-          ar[i]= sum;
-            System.out.print(sum);
+        int n = queries.length;
+        int[] prefixSum = calculatePrefixXor(arr);
+       
+        int[] res = new int[n];
+        for(int i=0;i<n;i++) {
+            res[i] = prefixSum[queries[i][1]]^(queries[i][0]== 0 ? 0 : (prefixSum[queries[i][0]-1]));
         }
-        return ar;
+        return res;
+    }
+
+    public int[] calculatePrefixXor(int[] arr) {
+        int n = arr.length;
+        int[] prefix = new int[n];
+        prefix[0] = arr[0];
+        for(int i=1;i<n;i++) {
+           prefix[i]=arr[i]^prefix[i-1];
+        }
+        return prefix;
     }
 }
