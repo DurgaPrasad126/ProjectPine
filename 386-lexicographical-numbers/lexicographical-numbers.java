887794f6b30c1dd35eb1abfbb357355772e6c2c9
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-09-21 17:31:39 -0600

    Time: 5 ms (42.99%) | Memory: 48.6 MB (37.47%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -1,15 +1,18 @@
 class Solution {
     public List<Integer> lexicalOrder(int n) {
-        String[] numbers = new String[n];
-        for(int i=1;i<=n;i++) {
-            numbers[i-1] = String.valueOf(i);
-        }
-        Arrays.sort(numbers);
-        List<Integer> nums = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            nums.add(Integer.valueOf(numbers[i]));
-        }
-        return nums;
+        int number  = 1;
+        List<Integer> lexicalNums = new ArrayList<>();
 
+        for(int i=0;i<n;i++){
+            lexicalNums.add(number);
+            if(number * 10 <= n) {
+                number*=10;
+            }
+            else {
+                while(number%10 == 9 || number >= n) number/=10;
+                number+=1;
+            }
+        }
+        return lexicalNums;
     }
 }
