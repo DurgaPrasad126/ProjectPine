b57b3ab989ad3585d80e4c3f62458856047fa6a9
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-09-23 10:43:34 -0600

    Time: 52 ms (50.00%) | Memory: 69.4 MB (100.00%) - LeetSync

--- 3541-report-spam-message/report-spam-message.java
+++ 3541-report-spam-message/report-spam-message.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean reportSpam(String[] message, String[] bannedWords) {
-        Map<String, Integer> banned = new HashMap<>();
+        Set<String> banned = new HashSet<>();
         constructBannedMap(bannedWords, banned);
 
         return isSpam(message, banned);
     }
 
-    public void constructBannedMap(String[] bannedWords, Map<String, Integer> banned) {
-        for(String word : bannedWords) banned.put(word, banned.getOrDefault(word, 0)+1);
+    public void constructBannedMap(String[] bannedWords, Set<String> banned) {
+        for(String word : bannedWords) banned.add(word);
     }
 
-    public boolean isSpam(String[] message, Map<String, Integer> banned) {
+    public boolean isSpam(String[] message, Set<String> banned) {
         int matchedWords = 0;
         for(String word : message) {
-            if(banned.containsKey(word)) {
+            if(banned.contains(word)) {
                 if(matchedWords == 1) return true;
                 else {
                     matchedWords++;
