b5acb720a9a88a22b63e355c7e76fd513cc51308
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-10-04 09:18:18 -0600

    Time: 34 ms (42.28%) | Memory: 56.9 MB (96.91%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        //return new int[0];
+        Set<Integer> uniqueEle = new HashSet<>();
+        for(int a : arr) uniqueEle.add(a);
+        List<Integer> ele = new ArrayList<>(uniqueEle);
+        Collections.sort(ele);
+        Map<Integer, Integer> rankArr = new HashMap<>();
+
+        for(int i=0;i<ele.size();i++) {
+            rankArr.put(ele.get(i), i+1);
+        }
+
+        int[] rankTra = new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            rankTra[i] = rankArr.get(arr[i]);
+        }
+        return rankTra;
+
+    }
+}
