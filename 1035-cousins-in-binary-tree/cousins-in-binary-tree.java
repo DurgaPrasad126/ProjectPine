ad0aaef5c509889f48cd3b30c2ba421f37abd454
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-10-23 18:53:49 -0600

    Time: 0 ms (100.00%) | Memory: 41.1 MB (83.78%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
@@ -20,8 +20,8 @@ class Solution {
         int[] yParent = new int[2];
         findParent(root, x, xParent, 0);
         findParent(root, y, yParent, 0);
-        System.out.println(xParent[0] + " --- " + xParent[1]);
-        System.out.println(yParent[0] + " --- " + yParent[1]);
+        //System.out.println(xParent[0] + " --- " + xParent[1]);
+        //System.out.println(yParent[0] + " --- " + yParent[1]);
         return (xParent[1] == yParent[1]) && (xParent[0] != yParent[0]);
     }
 
@@ -31,7 +31,7 @@ class Solution {
             //parent = new int[]{root.val, depth};
             parent[0] = root.val;
             parent[1] = depth;
-            System.out.println("visited" + " " + root.val + " " + depth);
+            //System.out.println("visited" + " " + root.val + " " + depth);
             return;
         }
         findParent(root.left, node, parent, depth+1);
