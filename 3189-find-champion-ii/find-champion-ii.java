684d275ab1d234c300fb03d4d626838bf45aeb78
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-11-25 22:59:06 -0700

    Time: 8 ms (14.29%) | Memory: 46 MB (25.00%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.java
+++ 3189-find-champion-ii/find-champion-ii.java
@@ -2,7 +2,6 @@ class Solution {
     int[] isWeaker = new int[100];
     public int findChampion(int n, int[][] edges) {
         Map<Integer, List<Integer>> adjList = constructAdjList(edges);
-        System.out.println(adjList);
         if(n-adjList.size() > 1) return -1;
 
         for(int i=0;i<n;i++) {
