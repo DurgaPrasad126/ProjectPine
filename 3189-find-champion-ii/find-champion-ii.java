1f58a0c6379d6b44beb14f3573c0558f29f7e36a
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-11-26 00:08:03 -0700

    Time: 7 ms (14.29%) | Memory: 46.2 MB (8.82%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.java
+++ 3189-find-champion-ii/find-champion-ii.java
@@ -1,15 +1,21 @@
 class Solution {
+    int[] isWeaker = new int[100];
     public int findChampion(int n, int[][] edges) {
-        int[] isWeaker = new int[n];
-        int result = -1;
-        for(int[] edge : edges){
-            isWeaker[edge[1]]++;
+        Map<Integer, List<Integer>> adjList = constructAdjList(edges);
+        if(n-adjList.size() > 1) return -1;
+
+        for(int i=0;i<n;i++) {
+            if(isWeaker[i] == 0) return i;
         }
-        for(int i = 0;i<n;i++){
-            if(isWeaker[i] == 0 && result != -1) return -1;
-            else if(isWeaker[i] == 0) result = i;
-        }
-        return result;
+        return -1;
     }
 
+    public Map<Integer, List<Integer>> constructAdjList(int[][] edges) {
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
+        for(int[] edge : edges) {
+            adjList.computeIfAbsent(edge[1], l -> new ArrayList<>()).add(edge[0]);
+            isWeaker[edge[1]] = 1;
+        }
+        return adjList;
+    }
 }
