33e2fa038877e56b4797dc1e56fe57a901228879
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-11-27 22:53:36 -0700

    Time: 188 ms (38.57%) | Memory: 45.7 MB (25.68%) - LeetSync

---
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
+        Map<Integer, Set<Integer>> adjList = new HashMap<>();
+        for(int i=0;i<n-1;i++){
+            adjList.computeIfAbsent(i, l -> new HashSet<>()).add(i+1);
+        }
+
+        int[] res = new int[queries.length];
+        int pos = 0;
+
+        for(int[] query : queries) {
+            adjList.get(query[0]).add(query[1]);
+            res[pos++] = getShortestPath(adjList, n);
+        }
+        return res;
+    }
+
+    public int getShortestPath(Map<Integer, Set<Integer>> map, int n) {
+        int min = Integer.MAX_VALUE;
+        Set<Integer> visited = new HashSet<>();
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0,0});
+
+        while(!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            if(cur[0] == 0 && map.get(cur[0]).contains(n-1)) return 1;
+            for(int node : map.get(cur[0])) {
+                if(node == n-1) {
+                    min = Math.min(cur[1]+1, min);
+                    return min;
+                }
+                if(!visited.contains(node)){
+                queue.add(new int[]{node, cur[1]+1});
+                visited.add(node);
+                }
+            }
+        }
+        return -1;
+    }
+}
