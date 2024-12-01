c35d0fb1b026180b55c3002855e53e5aef8ba11f
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-11-30 21:27:18 -0700

    Time: 4 ms (14.78%) | Memory: 44.6 MB (5.78%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -4,8 +4,7 @@ class Solution {
         for(int i : arr) ele.put(i, ele.getOrDefault(i, 0)+1);
 
         for(int i : arr) {
-            if(i == 0 && ele.get(i) >= 2) return true; 
-            else if(i!=0 && ele.containsKey(2*i)) return true;
+            if((i == 0 && ele.get(i) >= 2) || (i!=0 && ele.containsKey(2*i))) return true;
         }
         return false;
     }
