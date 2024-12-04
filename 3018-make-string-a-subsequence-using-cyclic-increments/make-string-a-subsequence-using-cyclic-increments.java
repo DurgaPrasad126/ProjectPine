d1e19e8bdc2da2b5adc3b77672044860f8f37bf7
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-12-03 18:08:52 -0700

    Time: 5 ms (100.00%) | Memory: 45.8 MB (16.45%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.java
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.java
@@ -5,15 +5,15 @@ class Solution {
         char[] ch2 = str2.toCharArray();
         int len1 = ch1.length;
         int len2 = ch2.length;
-        for(int i=0;i<len1;i++) {
-            if(ch1[i] == 'z') ch1[i] = 'a';
-            else ch1[i] = (char)(ch1[i]+1);
-        }
+        //for(int i=0;i<len1;i++) {
+        //    if(ch1[i] == 'z') ch1[i] = 'a';
+        //    else ch1[i] = (char)(ch1[i]+1);
+        //}
         //System.out.println(new String(ch1));
 
         int pos2 = 0, pos1 = 0;
         while(pos2 < len2 && pos1 < len1) {
-            if(ch1[pos1] == ch2[pos2] || getPreviousChar(ch1[pos1]) == ch2[pos2]) {
+            if(ch1[pos1] == ch2[pos2] || getNextChar(ch1[pos1]) == ch2[pos2]) {
                 pos2++;
             }
             pos1++;
@@ -22,5 +22,9 @@ class Solution {
     }
     public char getPreviousChar(char ch){
         return ch == 'a' ? 'z' : (char) (ch-1);
+    }
+
+    public char getNextChar(char ch){
+        return ch == 'z' ? 'a' : (char) (ch+1);
     }
 }
