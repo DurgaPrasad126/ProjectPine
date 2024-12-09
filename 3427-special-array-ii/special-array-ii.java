a2bfd6d432dee6f5977a3e639607c58a18fef5cf
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-12-08 22:08:15 -0700

    Time: 357 ms (5.39%) | Memory: 107.1 MB (22.40%) - LeetSync

---
+++ 3427-special-array-ii/special-array-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean[] isArraySpecial(int[] nums, int[][] queries) {
+        int len = nums.length;
+        int[] sameParityCount = new int[len];
+        for(int i=1;i<len;i++){
+            sameParityCount[i] = sameParityCount[i-1] + (nums[i-1] % 2 == nums[i] % 2 ? 1 : 0);
+            System.out.print(sameParityCount[i] + " -- ");
+        }
+
+
+
+        boolean[] result = new boolean[queries.length];
+        for(int i=0;i<queries.length;i++){
+            if(sameParityCount[queries[i][0]] == sameParityCount[queries[i][1]]) result[i] = true;
+            else result[i] = false;
+        }
+        return result;
+    }
+}
