f7c31300ed8eff0b6d985a5e8013f2ec2b12f977
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2024-12-15 22:55:23 -0700

    Time: 3 ms (33.80%) | Memory: 45 MB (34.76%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
@@ -1,26 +1,16 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> a[0]-b[0] == 0 ? a[1]-b[1] : a[0]-b[0]);
-        int n = nums.length;
-        int[][] input = new int[n][2];
-
-        for(int i=0;i<n;i++) {
-            input[i][0] = nums[i];
-            input[i][1] = i;
-
-            minHeap.add(input[i]);
+        Queue<int[]> q = new PriorityQueue<>((a,b) -> a[0] == b[0] ? a[1]-b[1] : a[0] - b[0]);
+        for(int i=0;i<nums.length;i++){
+            q.add(new int[]{nums[i], i});
         }
 
         while(k > 0) {
-            int[] curMinNum = minHeap.poll();
-            curMinNum[0] = curMinNum[0]*multiplier;
-            minHeap.add(curMinNum);
+            int[] cur = q.poll();
+            cur[0]*=multiplier;
+            nums[cur[1]] = cur[0];
+            q.add(cur);
             k--;
-        }
-
-        while(!minHeap.isEmpty()) {
-            int[] curEle = minHeap.poll();
-            nums[curEle[1]] = curEle[0];
         }
         return nums;
     }
