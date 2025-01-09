544314948c58d3d16ec60fd72eaecf149b78ec21
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2025-01-08 22:49:48 -0700

    Time: 0 ms (100.00%) | Memory: 42.8 MB (82.60%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
@@ -1,9 +1,8 @@
 class Solution {
     public int prefixCount(String[] words, String pref) {
-       // String[] words = sentence.split(" ");
-        int count=0;
-        for(int i=0;i<words.length;i++){
-            if(words[i].startsWith(pref)) count++;
+        int count = 0;
+        for(String word : words) {
+            count+=(word.startsWith(pref) ? 1 : 0);
         }
         return count;
     }
