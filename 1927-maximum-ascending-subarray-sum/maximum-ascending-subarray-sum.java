df9b122e9c099056b1bdc150e0b90f62b260087f
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2025-02-03 21:29:19 -0700

    Time: 9 ms (2.26%) | Memory: 41.6 MB (29.14%) - LeetSync

---
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int maxSum = 0;
+        int curSum = nums[0];
+        int n = nums.length;
+
+        for(int i=0;i<n-1;i++) {
+            if(nums[i] >= nums[i+1]) {
+                maxSum = Math.max(maxSum, curSum);
+                System.out.println(maxSum);
+                curSum = nums[i+1];
+            }
+            else {
+                curSum+=nums[i+1];
+            }
+        }
+        return Math.max(maxSum, curSum);
+    }
+}
