3e24e1cf5c38dbb12484a2e0389ce51c0ce99bc5
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2025-02-04 00:06:27 -0700

    Time: 10 ms (22.34%) | Memory: 86.1 MB (26.64%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
@@ -6,18 +6,18 @@ class Solution {
         vowels.add('i');
         vowels.add('o');
         vowels.add('u');
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] map = new int[100000];
         int vowelStrCount = 0;
         for(int i=0;i<words.length;i++) {
             char[] word = words[i].toCharArray();
            
             if(vowels.contains(word[0]) 
                 && vowels.contains(word[word.length-1])) {
-                    vowelStrCount++;
-                    map.put(i, vowelStrCount);
+                    
+                    map[i] = ++vowelStrCount;
                 }
             else {
-                map.put(i, vowelStrCount);
+                map[i] = vowelStrCount;
             }
         }
 
@@ -25,7 +25,7 @@ class Solution {
         int pos = 0;
 
         for(int[] query : queries) {
-            count[pos++] = map.get(query[1]) - (query[0] != 0 ? map.get(query[0]-1) : 0);
+            count[pos++] = map[query[1]] - (query[0] != 0 ? map[query[0]-1] : 0);
         }
         return count;
     }
