3f44856243579dcc6d14c3627ec9484e2eafde10
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2025-02-12 21:16:34 -0700

    Time: 185 ms (5.35%) | Memory: 70.5 MB (80.25%) - LeetSync

---
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int minOperations = 0;
+        PriorityQueue<Long> minHeap = new PriorityQueue(Comparator.naturalOrder());
+        for(int num : nums) minHeap.offer(1l*num);
+
+        while(minHeap.size() >= 2 && minHeap.peek() < k) {
+            long el1 = minHeap.poll();
+            long el2 = minHeap.poll();
+            minHeap.offer(Math.min(el1, el2)*2 + Math.max(el1,el2));
+            minOperations++;
+        }
+
+        return minOperations;
+    }
+}
