8d1a95dcc1e3d02b3c8a07d1776ba34a228538fa
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2025-03-14 22:00:49 -0600

    Time: 29 ms (18.77%) | Memory: 126.5 MB (27.51%) - LeetSync

---
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+    int[] nums;
+    int[][] queries;
+    public int minZeroArray(int[] nums, int[][] queries) {
+        int sum = 0;
+        for(int num : nums) sum+=num;
+        if(sum == 0) return 0;
+        this.nums = nums;
+        this.queries = queries;
+        int n = queries.length;
+        int low = 0;
+        int high = n-1;
+        int minK = -1;
+
+        while(low<=high){
+            int mid = low +(high-low)/2;
+            if(isZeroArray(mid)) {
+                high = mid-1;
+                minK = mid+1;
+            }
+            else {
+                low = mid+1;
+            }
+        }
+        return minK;
+    }
+
+    public boolean isZeroArray(int k) {
+        int n = nums.length;
+        int[] prefix = new int[n];
+
+        for(int i=0;i<=k;i++) {
+            prefix[queries[i][0]] += queries[i][2];
+            if(queries[i][1] != n-1) prefix[queries[i][1]+1] -= queries[i][2];
+        }
+
+        for(int i=0;i<n;i++){
+            if(i==0) {
+                if(prefix[i] < nums[i]) return false;
+            }
+            else {
+                prefix[i]+=prefix[i-1];
+                if(prefix[i] < nums[i]) return false;
+            }
+        }
+        return true;
+    }
+}
