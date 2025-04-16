d16c5a3db4ba51e33ef192e05ca03a8c536455f1
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2025-04-16 01:02:27 -0600

    Time: 42 ms (74.61%) | Memory: 46.8 MB (63.93%) - LeetSync

--- 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.java
+++ 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.java
@@ -2,9 +2,9 @@ class Solution {
     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
         Map<String, Integer> subArrCount = new HashMap<>();
         int n = s.length();
-        for(int i=minSize;i<=maxSize;i++) {
-            for(int j=0;j<=n-i;j++) {
-                String sub = s.substring(j, j+i);
+       // for(int i=minSize;i<=maxSize;i++) {
+            for(int j=0;j<=n-minSize;j++) {
+                String sub = s.substring(j, j+minSize);
                 if(subArrCount.containsKey(sub)) {
                     subArrCount.put(sub, subArrCount.get(sub) + 1);
                     continue;
@@ -13,7 +13,7 @@ class Solution {
                     subArrCount.put(sub, subArrCount.getOrDefault(sub, 0) + 1);
                 }
             }
-        }
+        //}
         int maxOcc = 0;
         for(String key : subArrCount.keySet()) {
             maxOcc = Math.max(maxOcc, subArrCount.get(key));
