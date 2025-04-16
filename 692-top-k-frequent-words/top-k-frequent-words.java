e8351acd8147a5518af0e0c54992004688e1a213
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2025-04-16 02:36:54 -0600

    Time: 6 ms (98.48%) | Memory: 45.1 MB (12.83%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.java
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -4,17 +4,18 @@ class Solution {
         for(String word : words) freq.put(word, freq.getOrDefault(word, 0) + 1);
         List<Word> wordss = new ArrayList<>();
         for(String word : freq.keySet()) wordss.add(new Word(word, freq.get(word)));
-        PriorityQueue<Word> minHeap = new PriorityQueue<>((a,b) -> b.count==a.count ? a.word.compareTo(b.word) : b.count-a.count);
+        PriorityQueue<Word> minHeap = new PriorityQueue<>((a,b) -> b.count==a.count ? b.word.compareTo(a.word) : a.count-b.count);
         for(Word word : wordss) {
+            // minHeap.offer(word);
             minHeap.offer(word);
-            // if(minHeap.size() == k) minHeap.poll();
-            // if(minHeap.isEmpty() || minHeap.peek().count < word.count) minHeap.offer(word);
+            if(minHeap.size() > k) minHeap.poll();
         }
         List<String> res = new ArrayList<>();
         while(k > 0 && !minHeap.isEmpty()) {
             res.add(minHeap.poll().word);
             k--;
         }
+        Collections.reverse(res);
         return res;
     }
     class Word {
