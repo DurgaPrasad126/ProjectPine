52cadfd53b27c49417c05ff644523e9a0642315a
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2025-04-15 23:54:34 -0600

    Time: 3 ms (100.00%) | Memory: 63.9 MB (59.90%) - LeetSync

--- 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.java
+++ 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.java
@@ -1,15 +1,14 @@
 class Solution {
     public int[] occurrencesOfElement(int[] nums, int[] queries, int x) {
-        Map<Integer, Integer> countToIdx = new HashMap<>();
         int count = 0;
         for(int i=0;i<nums.length;i++) {
-            if(nums[i] == x) countToIdx.put(++count, i);
+            if(nums[i] == x) nums[count++] = i;
         }
         int n = queries.length;
         int[] res = new int[n];
         for(int i=0;i<n;i++) {
-            if(countToIdx.containsKey(queries[i])) {
-                res[i] = countToIdx.get(queries[i]);
+            if(queries[i] <= count) {
+                res[i] = nums[queries[i]-1];
             }
             else res[i] = -1;
         }
