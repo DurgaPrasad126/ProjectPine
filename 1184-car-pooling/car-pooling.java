27bcbf4c61d0ed64f78401e80a716f519b6b5b5e
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2025-04-17 00:44:47 -0600

    Time: 2 ms (65.00%) | Memory: 44.7 MB (25.24%) - LeetSync

--- 1184-car-pooling/car-pooling.java
+++ 1184-car-pooling/car-pooling.java
@@ -1,23 +1,16 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        int n = trips.length;
-        int maxLoc = Integer.MIN_VALUE;
-
-        for(int[] trip : trips) maxLoc = Math.max(maxLoc, trip[2]);
-
-        int[] cumuCap = new int[maxLoc + 2];
-
-        for(int i=0; i<n; i++){
-            cumuCap[trips[i][1]] += trips[i][0];
-            if(trips[i][2] < maxLoc) cumuCap[trips[i][2]] -= trips[i][0];
+        int[] travel = new int[1002];
+        for(int i=0;i<trips.length;i++) {
+            travel[trips[i][1]] += trips[i][0];
+            travel[trips[i][2]] -= trips[i][0];
         }
 
-        for(int i=0;i<=maxLoc;i++){
-
-            if(i!=0) cumuCap[i]+=cumuCap[i-1];
-            //System.out.print(cumuCap[i] + " --- ");
-            if(cumuCap[i] > capacity) return false;
+        for(int i=1;i<travel.length;i++) {
+            travel[i]+=travel[i-1];
         }
+
+        for(int i=0;i<travel.length;i++) if(capacity < travel[i]) return false;
         return true;
     }
 }
