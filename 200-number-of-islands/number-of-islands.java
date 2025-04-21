431e2d79192f0c5402e349abe946f909e9ddf76c
Durga Prasad Alakunta <41814922+DurgaPrasad126@users.noreply.github.com>
2025-04-21 13:55:29 -0600

    Time: 5 ms (32.77%) | Memory: 52.5 MB (14.82%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,14 +1,14 @@
 class Solution {
     int[][] directions = new int[][]{{0,1}, {1,0}, {0,-1}, {-1,0}};
-    boolean[][] visited;
+   // boolean[][] visited;
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        visited = new boolean[m][n];
+        //visited = new boolean[m][n];
         int islandsCount = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++) {
-                if(!visited[i][j] && grid[i][j] == '1') {
+                if(grid[i][j] == '1') {
                     islandsCount += bfs(grid, m, n, i, j);
                 }
             }
@@ -28,9 +28,10 @@ class Solution {
                 for(int d = 0;d<directions.length;d++) {
                     int newX = directions[d][0] + curGrid[0];
                     int newY = directions[d][1] + curGrid[1];
-                    if(newX >= 0 && newY >= 0 && newX < m && newY < n && !visited[newX][newY] && grid[newX][newY] == '1' ) {
+                    if(newX >= 0 && newY >= 0 && newX < m && newY < n && grid[newX][newY] == '1' ) {
                         q.add(new int[]{newX, newY});
-                        visited[newX][newY] = true;
+                        grid[newX][newY] = '0';
+                       // visited[newX][newY] = true;
                     }
                 }
             }
